73302c68854b8772f3fbec06f76462d263e392b4
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-02-14 14:30:48 +0000

    Revert "davinci: switch back to AIDL power hal"
    This reverts commit 17169cd16ce34bf6723a2d4ae482f6022352c150.

--- device.mk
+++ device.mk
@@ -303,7 +303,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.3-service.sm6150-libperfmgr \
+    powerhint.json
 
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
@@ -352,7 +353,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
